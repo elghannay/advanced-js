2bbb6696e9ff3d7795447fd448a8b69b192b50fd
elghannay <mohamedelghannay@gmail.com>
2020-10-21 19:38:16 +0100

    framework init

---
+++ jQuery/app.js
@@ -0,0 +1,2 @@
+const test = G_('hello', 'there', 'en')
+console.log(test);
--- jQuery/greetr.js
+++ jQuery/greetr.js
@@ -1,1 +1,23 @@
-// an attempt to create a simple framework from scratch
+// first create a new execution context for our library so our global variables are safe,
+// and we are only exposing to the global object what we want.
+(function (global, $) {
+
+    const Greetr = function (name, lastName, language) {
+        return new Greetr.init(name, lastName, language)
+    }
+
+    Greetr.prototype = {};
+
+    Greetr.init = function (name, lastName, language) {
+        let that = this;
+        that.name = name || '';
+        that.lastName = lastName || '';
+        that.language = language || 'en';
+    }
+
+    Greetr.init.prototype = Greetr.prototype;
+
+    global.Greetr = global.G_ = Greetr;
+
+
+})(window, jQuery)
