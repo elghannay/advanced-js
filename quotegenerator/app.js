21d084bcda15479bbf9678ce11db19017c9aa323
elghannay <mohamedelghannay@gmail.com>
2020-10-23 03:13:56 +0100

    CORS issues

--- quotegenerator/app.js
+++ quotegenerator/app.js
@@ -16,7 +16,11 @@ function loadingComplete() {
         loader.hidden = true;
     }
 }
-// added proxy to overcome CORS problems with free api
+// added proxy to overcome CORS problems that come up when using free api  
+// and limit the access control allow origin, for a detail of the problem visit
+// https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9#:~:text=Why%20was%20the%20CORS%20error,%3A%20cross%2Dsite%20request%20forgery.
+// also may need to implement one and host it yourself to escape crowded proxy servers
+// https://github.com/Rob--W/cors-anywhere
 const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/';
 const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
@@ -45,6 +49,8 @@ async function getQuote() {
 const tweet = function () {
     const quote = quoteContent.innerText;
     const authorText = author.innerText;
+    // adding a text parameter to the link see docs:  
+    // https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/guides/web-intent
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${authorText}`;
     window.open(twitterUrl, '_blank');
 }
